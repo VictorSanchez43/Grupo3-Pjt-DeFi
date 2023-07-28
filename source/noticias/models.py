cbc8dabc084e0a9751de934990ebe29a90fa404a
Victor Sanchez <sanbombazos@gmail.com>
2023-07-27 22:26:30 -0300

    Cambio Templates

--- source/noticias/models.py
+++ source/noticias/models.py
@@ -4,9 +4,10 @@ from usuarios.models import Usuario
 # Create your models here.
 
 class Noticias(models.Model):
-    fecha = models.DateField(auto_now_add=True)
+    fecha = models.DateTimeField(auto_now_add=True)
     titulo = models.CharField(max_length=100)
     texto = models.TextField()
+    img = models.ImageField(upload_to='imagenes_noticias/', blank=True, null=True)
     uptdate = models.DateField(auto_now=True)
     autor = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='autor_noticias')
     
