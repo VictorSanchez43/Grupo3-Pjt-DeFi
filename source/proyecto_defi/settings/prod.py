10e2c0ca442a71c00ec711bfb22f7b96e8b0212d
Francisco E. Zalazar <zalazarfe@gmail.com>
2023-08-01 17:45:54 -0300

    Corrección del password de la bd de prod

--- source/proyecto_defi/settings/prod.py
+++ source/proyecto_defi/settings/prod.py
@@ -5,7 +5,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'franezal$default',
         'USER': 'franezal',
-        'PASSWORD': '08P@55w0rD12',
+        'PASSWORD': 'P@55w0rD',
         'HOST': 'franezal.mysql.pythonanywhere-services.com',
         'PORT': '3306',
     }
